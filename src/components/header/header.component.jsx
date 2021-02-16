091d505860565655a05b579d24248cd95834cabe
yucifer <yashjamesbond71@gmail.com>
2021-02-16 10:56:00 +0530

    Commit - 20 : updated some of the css with the use of styled-components library, especially in homepage and header components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import "./header.styles.scss";
+// import { Link } from "react-router-dom";
+// import "./header.styles.scss";
+
+import {HeaderContainer,OptionsContainer, OptionDiv, OptionLink, LogoContainer} from "./header.styles";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from '../../firebase/firebase.utils';
@@ -14,34 +16,34 @@ import { createStructuredSelector } from 'reselect';
 
 function Header({currentUser,hidden}) {
     return (
-        <div className="header">
-            <Link className="logo-container" to="/">
+        <HeaderContainer>
+            <LogoContainer to="/">
                 <Logo className="logo" />
-            </Link>
-            <div className="options">
-                <Link className="option" to="/shop">
+            </LogoContainer>
+            <OptionsContainer>
+                <OptionLink to="/shop">
                     SHOP
-                </Link>
-                <Link className="option" to="/shop">
+                </OptionLink>
+                <OptionLink className="option" to="/shop">
                     CONTACT
-                </Link>
+                </OptionLink>
                 {
                     currentUser 
                     ?
-                    <div className="option" onClick={() => auth.signOut()}>
+                    <OptionDiv onClick={() => auth.signOut()}>
                         SIGN OUT
-                    </div>
+                    </OptionDiv>
                     :
-                    <Link className="option" to="/signin"> SIGN IN </Link>
+                    <OptionLink className="option" to="/signin"> SIGN IN </OptionLink>
                 }
                 <CartIcon/>
-            </div>
+            </OptionsContainer>
             {
                 hidden ? null : 
                 <CartDropdown/>
             }
             
-        </div>
+        </HeaderContainer>
     )
 }
 
