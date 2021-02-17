8dd0c2a8912df3e17c1445d680272ca56eca8a91
yucifer <yashjamesbond71@gmail.com>
2021-02-17 13:14:29 +0530

    Commit - 21 : in order to make the application scalable for both mobile as well as web applications, pushed the shop data to the firestore backend, retrieved the data from the firestore to the front end and at last added the shop data fetched from the firestore to redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -44,6 +44,39 @@ firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+// Utility function to store our shop data to cloud firestore
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+  const batch = firestore.batch();
+
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+  return await batch.commit();
+};
+
+// Convert the snapshot from collections into right shape so collection-page and collection-overview can use it
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
+
 // Set up google authentication utility
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
