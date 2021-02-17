e35b340079a540a1e140e04da9b6088cc6472926
yucifer <yashjamesbond71@gmail.com>
2021-02-17 14:18:26 +0530

    Commit - 22 : added the HOC pattern for collection-overview and collection-page components in the Shop component, so that it can render a loading spinner is the data is not yet fetched from the firstore else it will render the component based on isLoading prop value if data required is fetched from firestore by shoppage component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,8 +6,17 @@ import CollectionPage from '../collection/collection.component';
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { updateCollections } from '../../redux/shop/shop.actions';
+import WithSpinner from '../../components/with-spinner/with-spinner.components';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
+
+    state = {
+        loading: true,
+    }
+
     unsubscribeFromSnapshot = null;
     componentDidMount() {
         const {updateCollections} = this.props;
@@ -15,15 +24,17 @@ class ShopPage extends React.Component {
         collectionRef.onSnapshot(async snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
+            this.setState({loading: false});
         })
     }
 
     render() {
         const {match} = this.props;
+        const {loading} = this.state;
         return (
             <div className="shop-page">
-               <Route exact path={`${match.path}`} component={CollectionsOverview} />
-               <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+               <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props}/>} />
+               <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />}/>
             </div>
         )
     }  
